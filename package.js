a9bb5c9f589b3bd699edc3e81b6268b81b267784
Colby Williams <colby@colbycheeze.com>
2015-11-16 06:48:44 -0600

    updated meteor package

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'colbycheeze:bluegrid',
   summary: 'A simple, responsive, and customizable grid based on the flex property',
-  version: '0.2.0',
+  version: '0.2.1',
   git: 'https://github.com/colbycheeze/bluegrid'
 });
 
@@ -9,11 +9,11 @@ Package.onUse(function(api) {
   api.versionsFrom('METEOR@1.2');
   api.use('fourseven:scss@3.4.1');
   var assets = [
-    "sass/_bluegrid.scss",
-    "sass/grid/_classes.scss",
-    "sass/grid/_mixins.scss",
-    "sass/helpers/_private.scss",
-    "sass/settings/_variables.scss",
+    "bluegrid/_bluegrid.scss",
+    "bluegrid/grid/_classes.scss",
+    "bluegrid/grid/_mixins.scss",
+    "bluegrid/helpers/_private.scss",
+    "bluegrid/settings/_variables.scss",
   ];
 
   api.addFiles(assets, 'client', { isImport: true });
